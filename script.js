4c320d037f9aa25f1beba8c7ceaedc265ab855b7
CodeMaster108 <clasurve@gmail.com>
2026-04-24 14:15:29 +0530

    script.js

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const elements = document.querySelectorAll('.fade');
+const text1 = document.getElementById("text1");
+const text2 = document.getElementById("text2");
+const text3 = document.getElementById("text3");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    }
-  });
-}, {
-  threshold: 0.2
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+
+  if (scroll < 500) {
+    text1.style.opacity = 1;
+    text2.style.opacity = 0;
+    text3.style.opacity = 0;
+  } 
+  else if (scroll < 1000) {
+    text1.style.opacity = 0;
+    text2.style.opacity = 1;
+    text3.style.opacity = 0;
+  } 
+  else {
+    text1.style.opacity = 0;
+    text2.style.opacity = 0;
+    text3.style.opacity = 1;
+  }
 });
-
-elements.forEach(el => observer.observe(el));
